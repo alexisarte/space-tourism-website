52c201bd2c6f823e10419c7556168616205d6b34
alexisarte <alexisrodriguezarteaga@gmail.com>
2023-08-06 22:19:53 -0300

    Add active menu indicator

--- src/components/NavLinkHeader.tsx
+++ src/components/NavLinkHeader.tsx
@@ -1,4 +1,5 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 interface Text {
   index: string;
@@ -6,13 +7,28 @@ interface Text {
 }
 
 const NavLinkHeader = ({ index, text }: Text) => {
+
+  const location = useLocation();
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    if (location.pathname === `/${text}`) {
+      setIsActive(true);
+    }
+  }, [location.pathname, text]);
+
   return (
-    <Link to={`/${text}`} className="group relative py-9">
-      <span>
+    <Link to={`/${text}`} className="group relative py-9" >
+      <p>
         <span className="font-bold inline-block sm:hidden lg:inline-block">{`${index}`}</span>
         {` ${text}`}
-      </span>
-      <span className="absolute bottom-0 block h-1 w-full scale-0 transition-all duration-700 group-hover:scale-100 group-hover:bg-white"></span>
+      </p>
+      <p
+        className={`absolute bottom-0 block h-1 w-full ${isActive ? 'bg-white'
+          : 'scale-0 transition-all duration-700 group-hover:scale-100 group-hover:bg-white'}`
+        }
+      >
+      </p>
     </Link>
   );
 };
