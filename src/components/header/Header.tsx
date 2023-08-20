d4008cbdf763e9e7cf60d04968b1182f8194401c
alexisarte <alexisrodriguezarteaga@gmail.com>
2023-08-19 21:09:06 -0300

    Correct header margin  on tablet devices

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -14,21 +14,19 @@ const Header = () => {
   };
 
   return (
-    <header className="flex justify-between items-center pl-11 pt-11">
+    <header className="flex justify-between items-center pl-11 pt-11 sm:pt-0 lg:pt-11">
       <img src={Logo} />
-      <hr className="relative left-10 z-10 w-[673px] h-px bg-white opacity-25  hidden lg:block" />
+      <hr className="relative left-10 z-10 w-[673px] h-px bg-white opacity-25 hidden lg:block" />
       <button className="relative z-20 sm:hidden pr-11" onClick={toogleMenu}>
         <img src={isOpen ? IconClose : IconHamburguer} />
       </button>
       <nav
-        className={
-          `sm:static sm:z-0 sm:flex sm:flex-row sm:justify-evenly sm:min-w-[450px] lg:min-w-[830px] sm:h-24 bg-white bg-opacity-5 backdrop-blur-[81.55px] tracking-[2.70px] uppercase 
-          sm:pt-0
-          ${isOpen
-            ? 'absolute top-0 right-0 z-10 flex flex-col gap-9 h-full w-3/4 pt-36 pl-8'
-            : 'hidden'
-          } `
-        }
+        className={`sm:static sm:z-0 sm:flex sm:flex-row sm:justify-evenly sm:min-w-[450px] lg:min-w-[830px] sm:h-24 bg-white bg-opacity-5 backdrop-blur-[81.55px] tracking-[2.70px] uppercase sm:pt-0
+          ${
+            isOpen
+              ? "absolute top-0 right-0 z-10 flex flex-col gap-9 h-full w-3/4 pt-36 pl-8"
+              : "hidden"
+          } `}
       >
         <NavLinkHeader index={"00"} text={"home"} path={""} />
         <NavLinkHeader index={"01"} text={"destination"} path={"destination"} />
